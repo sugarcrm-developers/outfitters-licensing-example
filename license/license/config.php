0642f7886d957911b6936bdaded19998b96f2b8d
Rafael Fernandes <rafael.fernandes@sugarcrm.com>
2024-11-13 14:27:19 -0500

    Updated sugaroutfitters.com to marketplace.sugarcrm.com

--- license/license/config.php
+++ license/license/config.php
@@ -2,9 +2,9 @@
 
 $outfitters_config = array(
     'name' => 'SampleLicenseAddon', //The matches the id value in your manifest file. This allow the library to lookup addon version from upgrade_history, so you can see what version of addon your customers are using
-    'shortname' => 'samplelicenseaddon', //The short name of the Add-on. e.g. For the url https://www.sugaroutfitters.com/addons/sugaroutfitters the shortname would be sugaroutfitters
+    'shortname' => 'samplelicenseaddon', //The short name of the Add-on. e.g. For the url https://marketplace.sugarcrm.com/addons/sugaroutfitters the shortname would be sugaroutfitters
     'public_key' => '', //The public key associated with the group
-    'api_url' => 'https://www.sugaroutfitters.com/api/v1',
+    'api_url' => 'https://marketplace.sugarcrm.com/api/v1',
     'validate_users' => false,
     'manage_licensed_users' => false, //Enable the user management tool to determine which users will be licensed to use the add-on. validate_users must be set to true if this is enabled. If the add-on must be licensed for all users then set this to false.
     'validation_frequency' => 'weekly', //default: weekly options: hourly, daily, weekly
--- license/license/language/fr_fr.lang.php
+++ license/license/language/fr_fr.lang.php
@@ -2,12 +2,12 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $license_strings = array (
-    'LBL_STEPS_TO_LOCATE_KEY_TITLE' => 'Pour retrouver la clé',
-    'LBL_STEPS_TO_LOCATE_KEY' => '1. Connectez-vous à <a href="https://www.sugaroutfitters.com" target="_blank">SugarOutfitters</a><br/>2. Allez à Account->Purchases</br>3. Recherchez la clé pour l\'achat de cet add-on<br/>4. Coller dans la boîte de clé de licence ci-dessous<br/>5. Cliquez sur "Validate"',
-    'LBL_LICENSE_KEY' => 'Clé de licence',
+    'LBL_STEPS_TO_LOCATE_KEY_TITLE' => 'Pour retrouver la clï¿½',
+    'LBL_STEPS_TO_LOCATE_KEY' => '1. Connectez-vous ï¿½ <a href="https://marketplace.sugarcrm.com" target="_blank">SugarOutfitters</a><br/>2. Allez ï¿½ Account->Purchases</br>3. Recherchez la clï¿½ pour l\'achat de cet add-on<br/>4. Coller dans la boï¿½te de clï¿½ de licence ci-dessous<br/>5. Cliquez sur "Validate"',
+    'LBL_LICENSE_KEY' => 'Clï¿½ de licence',
     'LBL_CURRENT_USERS' => 'Nombre actuel de l\'utilisateur',
     'LBL_LICENSED_USERS' => 'Licence Nombre d\'utilisateurs',
     'LBL_VALIDATE_LABEL' => 'Valider',
-    'LBL_VALIDATED_LABEL' => 'Validé',
+    'LBL_VALIDATED_LABEL' => 'Validï¿½',
 );
 
